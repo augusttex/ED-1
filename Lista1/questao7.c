f7ffa78d44e5ddf624e3557519688716f7d2de9c
Augusto <augustowrcc@gmail.com>
2026-02-28 19:17:11 -0400

    Faça um programa que declare três variáveis do tipo inteiro e três do tipo ponteiro de inteiro apontando para essas variáveis. Utilizando ponteiros, leia três números e os imprima em ordem crescente. O programa deve apresentar também o endereço de memória desses números.

---
+++ Lista1/questao7.c
@@ -0,0 +1,33 @@
+/*
+Nome: Augusto Castilho
+Matrícula: 54880
+Lista: 1
+Exercício: 2
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a, b, c;
+    int *x, *y, *z;
+    int t;
+
+    printf("Digite tres numeros inteiros: ");
+    scanf("%d %d %d", &a, &b, &c);  
+
+
+    x = &a;
+    y = &b;
+    z = &c; 
+
+    
+    if (*x > *y) { t = *x; *x = *y; *y = t; }
+    if (*y > *z) { t = *y; *y = *z; *z = t; }
+    if (*x > *y) { t = *x; *x = *y; *y = t; }
+
+    printf("Valores ordenados: %d %d %d\n", *x, *y, *z);
+
+    return 0;
+}
