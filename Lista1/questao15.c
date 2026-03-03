a7ddf7155f56440407c9e86ded0675b45bae1bb7
Augusto <augustowrcc@gmail.com>
2026-03-03 16:30:11 -0400

    Escreva um programa que receba um array de inteiros com 10 elementos digitados pelo usuário e encontre o menor (min) e o maior (max) elemento desse array. Utilize ponteiros tanto para acessar o array quanto para acessar as variáveis min e max, ou seja, são necessários pelo menos 3 ponteiros

---
+++ Lista1/questao15.c
@@ -0,0 +1,40 @@
+/*
+Nome: Augusto Castilho
+Matrícula: 54880
+Lista: 1
+Exercício: 14
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int a[10];
+    int *p = a;
+    int min, max;
+    int *pmin = &min;
+    int *pmax = &max;
+
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("Digite o valor do elemento [%d]: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+
+    min = max = a[0];
+     for(int i = 1; i < 10; i++)
+     {
+        p++;
+        if(*p < *pmin) 
+            *pmin = *p;
+        if(*p > *pmax)
+            *pmax = *p;
+     }
+    printf("O menor valor: %d\n", *pmin);
+    printf("O maior valor: %d\n", *pmax);
+
+    return 0;
+
+}
