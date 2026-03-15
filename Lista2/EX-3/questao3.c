fe3f88a351553b01513889b31a2dbbeec0313748
Augusto <augustowrcc@gmail.com>
2026-03-15 16:46:56 -0400

    Faça um programa que receba do usuário o tamanho de uma string e chame uma função para alocar dinamicamente essa string. Em seguida, o usuário deverá informar o conteúdo dessa string. O programa imprime a string sem suas vogais.

--- Lista2/EX-3/questao3.c
+++ Lista2/EX-3/questao3.c
@@ -9,8 +9,41 @@ Exercício: 3
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *alocarString(int tamanho) {
+    char *s = malloc((tamanho + 1) * sizeof(char));
+    return s;
+}
 
 int main() {
     
+    int tam;
+
+    printf("Digite o tamanho da string: ");
+    scanf("%d", &tam);
+
+    getchar();
+    char *str = alocarString(tam);
+        if(str == NULL)
+        {
+        printf("Erro ao alocar memoria.\n");
+        return 1;
+        }
+
+    printf("Digite a string: ");
+    fgets(str, tam + 1, stdin);
+    str[strcspn(str, "\n")] = '\0';
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(strchr("aeiouAEIOU", str[i]) == NULL) {
+            printf("%c ", str[i]);
+        }
+    }
+    
+
+
+
     return 0;
 }
