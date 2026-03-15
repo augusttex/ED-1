f111f86de7437cd3979c75a264fa21cbb3c98725
Augusto <augustowrcc@gmail.com>
2026-03-15 17:12:22 -0400

    Escreva um programa que leia primeiro os 6 números gerados pela loteria e depois os 6 números do seu bilhete. O programa então compara quantos números o jogador acertou. Em seguida, ele aloca espaço para um vetor de tamanho igual a quantidade de números corretos e guarda os números corretos nesse vetor. Finalmente, o programa exibe os números sorteados e os seus números corretos.

--- Lista2/EX-5/questao5.c
+++ Lista2/EX-5/questao5.c
@@ -9,8 +9,53 @@ Exercício: 5
 
 
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
     
+    int loteria[6] = {1, 3, 5, 7, 9, 11};
+    int bilhete[6];
+    int acertos = 0;
+    int *corretos; 
+    int k = 0;
+
+    
+    for(int i = 0; i < 6; i++) {
+        printf("Digite o numero %d do seu bilhete: ", i + 1);
+        scanf("%d", &bilhete[i]);
+    }
+
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 6; j++) {
+            if(bilhete[i] == loteria[j]) {
+                acertos++;
+                
+                
+            }
+        }
+    }
+    corretos = (int *) malloc(acertos * sizeof(int));
+    for(int i = 0; i < 6; i++) {
+        for(int j = 0; j < 6; j++) {
+            if(bilhete[i] == loteria[j]) {
+                corretos[k++] = bilhete[i];
+            }
+        }
+    }
+    printf("\nNumeros corretos: ");
+    for (int i = 0; i < acertos; i++) {
+    printf("%d ", corretos[i]);
+    }
+    printf("\nNumeros sorteados: ");
+    for (int i = 0; i < 6; i++) {
+    printf("%d ", loteria[i]);
+    }
+
+    free(corretos);
+    corretos = NULL;
+
+
+
     return 0;
 }
