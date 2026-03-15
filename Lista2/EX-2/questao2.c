d93fe6e3789333a8c0cb9dcdf1de74f47192f62e
Augusto <augustowrcc@gmail.com>
2026-03-15 16:23:24 -0400

    Faça um programa que leia do usuário o tamanho de um vetor a ser lido e faça a alocação dinâmica de memória. Em seguida, leia do usuário seus valores e mostre quantos dos números são pares e quantos são ímpares.

--- Lista2/EX-2/questao2.c
+++ Lista2/EX-2/questao2.c
@@ -9,8 +9,43 @@ Exercício: 2
 
 
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
     
+    int tam;
+    int par = 0, impar = 0;
+
+    printf("Qual o tamanho do vetor X? ");
+    scanf("%d", &tam);
+
+    int *x = (int *) malloc(tam * sizeof(int));
+    if (x == NULL) {
+        printf("Erro ao alocar memoria.\n");
+        return 1; }
+
+
+    for (int i = 0; i < tam; i++) {
+        printf("Digite um numero inteiro: ");
+        scanf("%d", &x[i]);
+    }
+
+    for(int i = 0; i < tam; i++)
+    {
+        if(x[i] % 2 == 0)  
+            par++;
+        else
+            impar++;  
+    }
+
+    printf("Quantidade de numeros pares: %d\n", par);
+    printf("Quantidade de numeros impares: %d\n", impar);
+
+    free(x);
+    x = NULL;
+
+    
+
     return 0;
 }
