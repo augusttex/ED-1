7534ba2b31a9129183e22ef4cd9f3c069d4a4125
Augusto <augustowrcc@gmail.com>
2026-03-15 17:54:04 -0400

    Faça um programa que leia uma quantidade qualquer de números armazenando-os na memória e pare a leitura quando o usuário entrar um número negativo. Em seguida, imprima o vetor lido. Use a função realloc().

--- Lista2/EX-7/questao7.c
+++ Lista2/EX-7/questao7.c
@@ -9,8 +9,51 @@ Exercício: 7
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    
+int main(void) {
+    int *x = NULL;
+    int *t;
+    int n, qntd = 0;
+
+    while (1) {
+        printf("Digite um numero: ");
+        if (scanf("%d", &n) != 1) {
+            printf("Entrada invalida.\n");
+            free(x);
+            return 1;
+        }
+
+        if (n < 0) {
+            break;
+        }
+
+        t = realloc(x, (qntd + 1) * sizeof(int));
+        if (t == NULL) {
+            printf("Erro ao alocar memoria.\n");
+            free(x);
+            return 1;
+        }
+
+        x = t;
+        x[qntd] = n;
+        qntd++;
+    }
+
+    printf("Vetor lido: ");
+    for (int i = 0; i < qntd; i++) {
+        printf("%d ", x[i]);
+    }
+    printf("\n");
+
+    free(x);
+    x = NULL;
+
     return 0;
 }
+
+
+
+
+
+
