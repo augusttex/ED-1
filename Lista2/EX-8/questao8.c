9d20e052f7b5cbeda976a864fb19ccf9ff756a2a
Augusto <augustowrcc@gmail.com>
2026-03-15 21:42:58 -0400

    Faça um programa que pergunte ao usuário quantos valores ele deseja armazenar em um vetor de double, depois use a função malloc para reservar (alocar) o espaço de memória de acordo com o especificado pelo usuário. Esse vetor deve ter um tamanho maior ou igual a 10 elementos. Use este vetor dinâmico como um vetor comum, atribuindo aos 10 primeiros elementos do vetor valores aleatórios (usando a função rand()) entre 0 e 100. Exiba na tela os valores armazenados nos 10 primeiros elementos do vetor.

--- Lista2/EX-8/questao8.c
+++ Lista2/EX-8/questao8.c
@@ -9,8 +9,45 @@ Exercício: 8
 
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
+int main(void) {
     
+    int n;
+    double *vetor = NULL;
+
+    printf("Quantos valores deseja armazenar no vetor (minimo 10)? ");
+    if (scanf("%d", &n) != 1) {
+        printf("Entrada invalida.\n");
+        return 1;
+    }
+
+    if (n < 10) {
+        printf("O tamanho do vetor deve ser maior ou igual a 10.\n");
+        return 1;
+    }
+
+    vetor = (double *) malloc(n * sizeof(double));
+    if (vetor == NULL) {
+        printf("Erro ao alocar memoria.\n");
+        return 1;
+    }
+
+    srand((unsigned int) time(NULL));
+
+    for (int i = 0; i < 10; i++) {
+        vetor[i] = rand() % 101; /* valores entre 0 e 100 */
+    }
+
+    printf("\nValores nos 10 primeiros elementos:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("vetor[%d] = %.2f\n", i, vetor[i]);
+    }
+
+    free(vetor);
+    vetor = NULL;
+
+
     return 0;
 }
