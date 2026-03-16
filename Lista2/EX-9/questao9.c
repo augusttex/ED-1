76262b8ef5db3704f8a22b29d38db2f8fd19840b
Augusto <augustowrcc@gmail.com>
2026-03-15 21:44:50 -0400

    Crie um programa que declare uma estrutura (registro) para o cadastro de alunos. a) Deverão ser armazenados, para cada aluno: matrícula, sobrenome (apenas um) e ano de nascimento; b) Ao início do programa, o usuário deverá informar o número de alunos que serão armazenados; c) O programa deverá alocar dinamicamente a quantidade necessária de memória para armazenar os registros dos alunos; d) O programa deverá pedir ao usuário que entre com as informações dos alunos. e) Ao final, mostrar os dados armazenados e liberar a memória alocada.

--- Lista2/EX-9/questao9.c
+++ Lista2/EX-9/questao9.c
@@ -9,8 +9,70 @@ Exercício: 9
 
 
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int matricula;
+    char sobrenome[100];
+    int anoNascimento;
+} Aluno;
 
 int main() {
-    
+    int n;
+    Aluno *alunos = NULL;
+
+    printf("Informe a quantidade de alunos: ");
+    if (scanf("%d", &n) != 1) {
+        printf("Entrada invalida.\n");
+        return 1;
+    }
+
+    if (n <= 0) {
+        printf("A quantidade deve ser maior que zero.\n");
+        return 1;
+    }
+
+    alunos = (Aluno *) malloc(n * sizeof(Aluno));
+    if (alunos == NULL) {
+        printf("Erro ao alocar memoria.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("\nAluno %d\n", i + 1);
+
+        printf("Matricula: ");
+        if (scanf("%d", &alunos[i].matricula) != 1) {
+            printf("Entrada invalida para matricula.\n");
+            free(alunos);
+            return 1;
+        }
+
+        printf("Sobrenome: ");
+        if (scanf("%99s", alunos[i].sobrenome) != 1) {
+            printf("Entrada invalida para sobrenome.\n");
+            free(alunos);
+            return 1;
+        }
+
+        printf("Ano de nascimento: ");
+        if (scanf("%d", &alunos[i].anoNascimento) != 1) {
+            printf("Entrada invalida para ano de nascimento.\n");
+            free(alunos);
+            return 1;
+        }
+    }
+
+    printf("\n=== Dados dos alunos cadastrados ===\n");
+    for (int i = 0; i < n; i++) {
+        printf("Aluno %d\n", i + 1);
+        printf("Matricula: %d\n", alunos[i].matricula);
+        printf("Sobrenome: %s\n", alunos[i].sobrenome);
+        printf("Ano de nascimento: %d\n\n", alunos[i].anoNascimento);
+    }
+
+    free(alunos);
+    alunos = NULL;
+
     return 0;
 }
