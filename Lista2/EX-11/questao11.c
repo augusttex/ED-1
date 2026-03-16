06f34b71f6ccbf260191c4d5fc823f426f79e903
Augusto <augustowrcc@gmail.com>
2026-03-15 21:52:18 -0400

    Escreva um programa que aloque dinamicamente uma matriz (de inteiros) de dimensões definidas pelo usuário e a leia. Em seguida, implemente uma função que receba um valor, retorne 1 caso o valor esteja na matriz ou retorne 0 caso não esteja na matriz.

--- Lista2/EX-11/questao11.c
+++ Lista2/EX-11/questao11.c
@@ -9,8 +9,96 @@ Exercício: 11
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int buscarValorNaMatriz(int **matriz, int linhas, int colunas, int valor) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            if (matriz[i][j] == valor) {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main(void) {
     
+    int linhas, colunas;
+    int **matriz = NULL;
+    int valorBusca;
+
+    printf("Informe o numero de linhas: ");
+    if (scanf("%d", &linhas) != 1) {
+        printf("Entrada invalida.\n");
+        return 1;
+    }
+
+    printf("Informe o numero de colunas: ");
+    if (scanf("%d", &colunas) != 1) {
+        printf("Entrada invalida.\n");
+        return 1;
+    }
+
+    if (linhas <= 0 || colunas <= 0) {
+        printf("Linhas e colunas devem ser maiores que zero.\n");
+        return 1;
+    }
+
+    matriz = (int **) malloc(linhas * sizeof(int *));
+    if (matriz == NULL) {
+        printf("Erro ao alocar memoria para linhas.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (int *) malloc(colunas * sizeof(int));
+        if (matriz[i] == NULL) {
+            printf("Erro ao alocar memoria para colunas.\n");
+            for (int k = 0; k < i; k++) {
+                free(matriz[k]);
+            }
+            free(matriz);
+            return 1;
+        }
+    }
+
+    printf("\nDigite os elementos da matriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("matriz[%d][%d]: ", i, j);
+            if (scanf("%d", &matriz[i][j]) != 1) {
+                printf("Entrada invalida.\n");
+                for (int k = 0; k < linhas; k++) {
+                    free(matriz[k]);
+                }
+                free(matriz);
+                return 1;
+            }
+        }
+    }
+
+    printf("\nDigite o valor a buscar: ");
+    if (scanf("%d", &valorBusca) != 1) {
+        printf("Entrada invalida.\n");
+        for (int i = 0; i < linhas; i++) {
+            free(matriz[i]);
+        }
+        free(matriz);
+        return 1;
+    }
+
+    if (buscarValorNaMatriz(matriz, linhas, colunas, valorBusca) == 1) {
+        printf("Resultado: 1 (valor encontrado na matriz)\n");
+    } else {
+        printf("Resultado: 0 (valor nao encontrado na matriz)\n");
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+    matriz = NULL;
+
     return 0;
 }
