45229aa86253f7e833ca6c735bfb18b08b15df27
Augusto <augustowrcc@gmail.com>
2026-03-15 21:54:22 -0400

    Construa um programa que leia o número de linhas e de colunas de uma matriz de números reais, aloque espaço dinamicamente para esta e a inicialize com valores fornecidos pelo usuário. Ao final, o programa deverá retornar a matriz na saída padrão com layout apropriado.

--- Lista2/EX-12/questao12.c
+++ Lista2/EX-12/questao12.c
@@ -9,8 +9,76 @@ Exercício: 12
 
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(void) {
+    
+    int linhas, colunas;
+    double **matriz = NULL;
+
+    printf("Digite o numero de linhas: ");
+    if (scanf("%d", &linhas) != 1) {
+        printf("Entrada invalida.\n");
+        return 1;
+    }
+
+    printf("Digite o numero de colunas: ");
+    if (scanf("%d", &colunas) != 1) {
+        printf("Entrada invalida.\n");
+        return 1;
+    }
+
+    if (linhas <= 0 || colunas <= 0) {
+        printf("Linhas e colunas devem ser maiores que zero.\n");
+        return 1;
+    }
+
+    matriz = (double **) malloc(linhas * sizeof(double *));
+    if (matriz == NULL) {
+        printf("Erro ao alocar memoria para as linhas.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (double *) malloc(colunas * sizeof(double));
+        if (matriz[i] == NULL) {
+            printf("Erro ao alocar memoria para as colunas.\n");
+            for (int k = 0; k < i; k++) {
+                free(matriz[k]);
+            }
+            free(matriz);
+            return 1;
+        }
+    }
+
+    printf("\nDigite os valores da matriz:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("matriz[%d][%d]: ", i, j);
+            if (scanf("%lf", &matriz[i][j]) != 1) {
+                printf("Entrada invalida.\n");
+                for (int k = 0; k < linhas; k++) {
+                    free(matriz[k]);
+                }
+                free(matriz);
+                return 1;
+            }
+        }
+    }
+
+    printf("\nMatriz informada:\n");
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%8.2lf ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+    matriz = NULL;
     
     return 0;
 }
