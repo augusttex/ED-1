15813e722ce3750efab3f703563315d28574bb15
Augusto <augustowrcc@gmail.com>
2026-04-07 15:44:31 -0400

    2. O que significa alocação estática de memória para um conjunto de elementos?

--- Lista3/EX-2/questao2.c
+++ Lista3/EX-2/questao2.c
@@ -3,5 +3,9 @@ Nome: Augusto Castilho
 Matrícula: 54880
 Lista: 3
 Exercício: 2
-*/
 
+
+A alocação estática de memória significa que o espaço na memória para o conjunto de elementos é reservado em tempo de compilação (antes de o programa começar a rodar).
+
+Sua principal característica é que o tamanho da estrutura é fixo. Uma vez definido, a quantidade de memória alocada não pode aumentar ou diminuir durante a execução do programa, e esse espaço permanece ocupado e indisponível para outros usos até o fim do ciclo de vida da variável ou do escopo em que foi declarada.
+*/
