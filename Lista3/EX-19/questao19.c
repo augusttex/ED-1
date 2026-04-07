803595f5761706c5642a3f976cb70386203ea441
Augusto <augustowrcc@gmail.com>
2026-04-07 16:20:35 -0400

    Implemente uma rotina recursiva para calcular o tamanho de uma lista dinâmica e encadeada.

--- Lista3/EX-19/questao19.c
+++ Lista3/EX-19/questao19.c
@@ -5,3 +5,30 @@ Lista: 3
 Exercício: 19
 */
 
+#include <stdio.h>
+
+typedef struct No {
+	int valor;
+	struct No *prox;
+} No;
+
+int tamanhoListaRecursivo(const No *inicio) {
+	if (inicio == NULL) {
+		return 0;
+	}
+
+	return 1 + tamanhoListaRecursivo(inicio->prox);
+}
+
+int main(void) {
+	No n4 = {40, NULL};
+	No n3 = {30, &n4};
+	No n2 = {20, &n3};
+	No n1 = {10, &n2};
+	No *lista = &n1;
+
+	printf("Tamanho da lista: %d\n", tamanhoListaRecursivo(lista));
+
+	return 0;
+}
+
