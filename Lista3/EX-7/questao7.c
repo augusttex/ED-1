8bfeb26e1e245679c4f69263d1904fb1b975efdd
Augusto <augustowrcc@gmail.com>
2026-04-07 15:57:33 -0400

    Faça uma função para remover os n primeiros elementos de uma lista estática. A função deve retornar se a operação foi possível ou não.

--- Lista3/EX-7/questao7.c
+++ Lista3/EX-7/questao7.c
@@ -5,3 +5,61 @@ Lista: 3
 Exercício: 7
 */
 
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MAX_TAM 100
+
+typedef struct {
+	int dados[MAX_TAM];
+	int tamanho;
+} ListaEstatica;
+
+bool removerPrimeirosN(ListaEstatica *lista, int n) {
+	int i;
+
+	if (lista == NULL || n < 0 || n > lista->tamanho) {
+		return false;
+	}
+
+	for (i = n; i < lista->tamanho; i++) {
+		lista->dados[i - n] = lista->dados[i];
+	}
+
+	lista->tamanho -= n;
+	return true;
+}
+
+void imprimirLista(const ListaEstatica *lista) {
+	int i;
+
+	printf("[");
+	for (i = 0; i < lista->tamanho; i++) {
+		printf("%d", lista->dados[i]);
+		if (i < lista->tamanho - 1) {
+			printf(", ");
+		}
+	}
+	printf("]\n");
+}
+
+int main(void) {
+	ListaEstatica lista = {{10, 20, 30, 40, 50}, 5};
+	bool operacaoValida;
+
+	printf("Lista original: ");
+	imprimirLista(&lista);
+
+	if (removerPrimeirosN(&lista, 2)) {
+		printf("Remocao de 2 elementos: ");
+		imprimirLista(&lista);
+	} else {
+		printf("Nao foi possivel remover 2 elementos.\n");
+	}
+
+	operacaoValida = removerPrimeirosN(&lista, 10);
+	(void)operacaoValida;
+
+	return 0;
+}
+
