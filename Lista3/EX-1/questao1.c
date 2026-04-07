84cbca977fe5a3cd73ca0d144dc4aa0a600dede2
Augusto <augustowrcc@gmail.com>
2026-04-07 15:42:27 -0400

    1. O que significa alocação sequencial de memória para um conjunto de elementos?

--- Lista3/EX-1/questao1.c
+++ Lista3/EX-1/questao1.c
@@ -3,5 +3,9 @@ Nome: Augusto Castilho
 Matrícula: 54880
 Lista: 3
 Exercício: 1
-*/
 
+
+Alocação sequencial de memória significa que os elementos de um conjunto de dados são armazenados de forma contígua (um imediatamente ao lado do outro) em endereços consecutivos na memória física do computador.
+
+Essa organização permite o acesso direto a qualquer elemento utilizando apenas a sua posição (índice), como é o caso dos vetores (arrays).
+*/
