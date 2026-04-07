0ff726b6e48724fef3c1cacaa41b1d1fd86d0713
Augusto <augustowrcc@gmail.com>
2026-04-07 15:59:10 -0400

    Faça uma função para buscar a posição e o valor do maior elemento da lista estática, os quais deverão ser retornados por referência. A função deve retornar se a operação foi possível ou não.

--- Lista3/EX-8/questao8.c
+++ Lista3/EX-8/questao8.c
@@ -5,3 +5,47 @@ Lista: 3
 Exercício: 8
 */
 
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MAX_TAM 100
+
+typedef struct {
+	int dados[MAX_TAM];
+	int tamanho;
+} ListaEstatica;
+
+bool buscarMaior(const ListaEstatica *lista, int *posicaoMaior, int *valorMaior) {
+	int i;
+	int indiceMaior;
+
+	if (lista == NULL || posicaoMaior == NULL || valorMaior == NULL || lista->tamanho <= 0) {
+		return false;
+	}
+
+	indiceMaior = 0;
+
+	for (i = 1; i < lista->tamanho; i++) {
+		if (lista->dados[i] > lista->dados[indiceMaior]) {
+			indiceMaior = i;
+		}
+	}
+
+	*posicaoMaior = indiceMaior;
+	*valorMaior = lista->dados[indiceMaior];
+	return true;
+}
+
+int main(void) {
+	ListaEstatica lista = {{12, 8, 35, 17, 35, 4}, 6};
+	int posicao;
+	int valor;
+
+	if (buscarMaior(&lista, &posicao, &valor)) {
+		printf("Posicao do maior: %d\n", posicao);
+		printf("Valor do maior: %d\n", valor);
+	}
+
+	return 0;
+}
+
