c00c4d191957d8154933508fcc5ce746aaaee43a
Augusto <augustowrcc@gmail.com>
2026-04-07 15:49:52 -0400

    5. Qual a diferença entre alocação estática e alocação dinâmica?

--- Lista3/EX-5/questao5.c
+++ Lista3/EX-5/questao5.c
@@ -3,5 +3,17 @@ Nome: Augusto Castilho
 Matrícula: 54880
 Lista: 3
 Exercício: 5
+
+
+Alocação Estática: Ocorre em tempo de compilação (antes de o programa começar a rodar). O tamanho do espaço de memória reservado é fixo, ou seja, não pode crescer nem diminuir durante a execução do programa. A liberação desse espaço é feita automaticamente pelo sistema.
+
+Alocação Dinâmica: Ocorre em tempo de execução (enquanto o programa está rodando). O tamanho do espaço é flexível e alocado sob demanda, conforme a necessidade real do sistema naquele momento. O programador tem controle total e é responsável por solicitar essa memória e por liberá-la manualmente quando não for mais utilizá-la (usando funções como malloc e free na linguagem C, por exemplo).
+
+
+
+
+
+
+
 */
 
