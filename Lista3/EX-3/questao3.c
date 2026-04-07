32c32ffabb84614596c4c66e53875c254f72e597
Augusto <augustowrcc@gmail.com>
2026-04-07 15:45:35 -0400

    3. Qual a diferença entre alocação sequencial e alocação encadeada?

--- Lista3/EX-3/questao3.c
+++ Lista3/EX-3/questao3.c
@@ -3,5 +3,17 @@ Nome: Augusto Castilho
 Matrícula: 54880
 Lista: 3
 Exercício: 3
+
+
+A principal diferença está na organização física dos elementos na memória e em como eles são acessados:
+
+Alocação Sequencial: Os elementos são armazenados em endereços de memória contíguos (um exatamente ao lado do outro). Isso permite o acesso direto a qualquer dado através de um índice, mas torna o tamanho da estrutura mais rígido e operações de inserção/remoção no meio dela mais custosas.
+
+Alocação Encadeada: Os elementos ficam espalhados na memória. Para mantê-los conectados, cada elemento possui um ponteiro que guarda o endereço de memória do próximo elemento da sequência. Isso torna o tamanho da estrutura dinâmico e facilita inserções/remoções, mas exige acesso sequencial (é preciso percorrer os elementos um por um desde o início para encontrar um dado específico).
+
+
+
+
+
 */
 
