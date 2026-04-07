adcb771c4e8fb385b28ab57ec7c646801d5e2bb0
Augusto <augustowrcc@gmail.com>
2026-04-07 16:12:07 -0400

    15. Escreva uma função que copie uma lista encadeada para um vetor.

--- Lista3/EX-15/questao15.c
+++ Lista3/EX-15/questao15.c
@@ -5,3 +5,50 @@ Lista: 3
 Exercício: 15
 */
 
+#include <stdio.h>
+
+typedef struct No {
+	int valor;
+	struct No *prox;
+} No;
+
+int copiarListaParaVetor(const No *inicio, int vetor[], int capacidade) {
+	int i = 0;
+	const No *atual = inicio;
+
+	if (vetor == NULL || capacidade <= 0) {
+		return 0;
+	}
+
+	while (atual != NULL && i < capacidade) {
+		vetor[i] = atual->valor;
+		i++;
+		atual = atual->prox;
+	}
+
+	return i;
+}
+
+int main(void) {
+	No n4 = {40, NULL};
+	No n3 = {30, &n4};
+	No n2 = {20, &n3};
+	No n1 = {10, &n2};
+	int vetor[10];
+	int quantidade;
+	int i;
+
+	quantidade = copiarListaParaVetor(&n1, vetor, 10);
+
+	printf("Elementos copiados para o vetor: [");
+	for (i = 0; i < quantidade; i++) {
+		printf("%d", vetor[i]);
+		if (i < quantidade - 1) {
+			printf(", ");
+		}
+	}
+	printf("]\n");
+
+	return 0;
+}
+
