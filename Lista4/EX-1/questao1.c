0f0b6889e36374e51170843def6df386c1795e3c
Augusto <augustowrcc@gmail.com>
2026-04-10 18:39:31 -0400

    1. O que é e como funciona uma estrutura do tipo fila?

--- Lista4/EX-1/questao1.c
+++ Lista4/EX-1/questao1.c
@@ -3,4 +3,14 @@ Nome: Augusto Castilho
 Matrícula: 54880
 Lista: 4
 Exercício: 1
+
+Uma fila é uma estrutura de dados linear que segue o princípio FIFO (First-In, First-Out - o primeiro a entrar é o primeiro a sair).
+
+Seu funcionamento se baseia em duas regras simples:
+
+Inserção: Novos elementos são sempre adicionados no final da fila.
+
+Remoção: Apenas o elemento que está no início da fila pode ser removido.
+
+
 */ 
