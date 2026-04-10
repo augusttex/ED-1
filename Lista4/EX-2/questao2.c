3da10d67294c6e89da578e0a433ed69e7f68aa11
Augusto <augustowrcc@gmail.com>
2026-04-10 18:40:36 -0400

    Em que situações uma fila pode ser utilizada?

--- Lista4/EX-2/questao2.c
+++ Lista4/EX-2/questao2.c
@@ -3,4 +3,18 @@ Nome: Augusto Castilho
 Matrícula: 54880
 Lista: 4
 Exercício: 2
+
+Uma fila deve ser utilizada em qualquer situação onde é necessário garantir que o processamento de dados, tarefas ou requisições ocorra na ordem exata de chegada (priorizando os mais antigos).
+
+Exemplos práticos de aplicação incluem:
+
+Gerenciamento de Tarefas no Sistema Operacional: Organizar a ordem dos programas ou processos que estão aguardando tempo de processamento na CPU.
+
+Filas de Impressão (Spooling): Garantir que os documentos enviados para uma impressora sejam impressos na sequência em que os cliques ocorreram.
+
+Buffers de Dados: Armazenamento temporário para transferência de dados entre componentes que operam em velocidades diferentes (ex: o buffer que carrega um vídeo no YouTube antes de você assistir).
+
+Servidores e Sistemas Web: Gerenciamento do fluxo de requisições de clientes quando o servidor atinge sua capacidade máxima de processamento simultâneo (ex: fila de espera para comprar ingressos de um show online).
+
+Algoritmos de Busca: Estrutura base para a execução de buscas em largura (BFS - Breadth-First Search) na travessia de grafos ou árvores em programação.
 */
