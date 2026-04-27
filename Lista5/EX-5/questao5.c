2057bf15f74fb0c45b51250052b705f06b34bf4e
Augusto <augustowrcc@gmail.com>
2026-04-27 17:48:15 -0400

    Considere uma pilha que armazene caracteres. Escreva uma função que verifique se uma palavra é um palíndromo.

--- Lista5/EX-5/questao5.c
+++ Lista5/EX-5/questao5.c
@@ -6,3 +6,28 @@ Exercício: 5
 
 */
 
+#include <stdio.h>
+#include <string.h>
+
+char pilha[100];
+int topo = -1;
+
+void push(char c) { pilha[++topo] = c; }
+char pop() { return pilha[topo--]; }
+
+int palindromo(char *palavra) {
+    int tamanho = strlen(palavra);
+    topo = -1;
+    
+    for (int i = 0; i < tamanho; i++) {
+        push(palavra[i]);
+    }
+    
+    for (int i = 0; i < tamanho; i++) {
+        if (palavra[i] != pop()) {
+            return 0; 
+        }
+    }
+    
+    return 1; 
+}
