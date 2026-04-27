1207dd82123f706497319aca76e1ad15bb75d108
Augusto <augustowrcc@gmail.com>
2026-04-27 17:51:20 -0400

    Dado uma pilha que armazene números, escreva uma função para ordenar os valores da pilha em ordem crescente.

--- Lista5/EX-7/questao7.c
+++ Lista5/EX-7/questao7.c
@@ -6,3 +6,48 @@ Exercício: 7
 
 */
 
+#include <stdio.h>
+
+int pilha[100];
+int topo = -1;
+
+void push(int v) { pilha[++topo] = v; }
+int pop() { return pilha[topo--]; }
+int vazia() { return topo == -1; }
+
+void ordenar() {
+    int pilha_aux[100];
+    int topo_aux = -1;
+
+    while (!vazia()) {
+        int temp = pop();
+
+        while (topo_aux != -1 && pilha_aux[topo_aux] > temp) {
+            push(pilha_aux[topo_aux--]);
+        }
+
+        pilha_aux[++topo_aux] = temp;
+    }
+
+    while (topo_aux != -1) {
+        push(pilha_aux[topo_aux--]);
+    }
+}
+
+int main() {
+    push(34);
+    push(3);
+    push(31);
+    push(98);
+    push(92);
+    push(23);
+
+    ordenar();
+
+    while (!vazia()) {
+        printf("%d ", pop());
+    }
+    printf("\n");
+
+    return 0;
+}
