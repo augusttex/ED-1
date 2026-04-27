01859055b229cea1a56fbb86ff2c886cdc548180
Augusto <augustowrcc@gmail.com>
2026-04-27 17:50:13 -0400

    Escreva um programa que utilize uma pilha para verificar se expressões aritméticas estão com a parentização correta. O programa deve verificar expressões para ver se cada “abre parenteses” tem um “fecha parenteses” correspondente. Ex.: Correto: ( ( ) ) – ( ( )( ) ) – ( ) ( ) Incorreto: )( – ( ( ) ( – ) ) ( (

--- Lista5/EX-6/questao6.c
+++ Lista5/EX-6/questao6.c
@@ -6,3 +6,50 @@ Exercício: 6
 
 */
 
+#include <stdio.h>
+
+char pilha[100];
+int topo = -1;
+
+void push(char c) { pilha[++topo] = c; }
+char pop() { return pilha[topo--]; }
+int vazia() { return topo == -1; }
+
+int verifica_parenteses(char *expressao) {
+    topo = -1; 
+    
+    for (int i = 0; expressao[i] != '\0'; i++) {
+        if (expressao[i] == '(') {
+            push('(');
+        } 
+        else if (expressao[i] == ')') {
+            if (vazia()) {
+                return 0; 
+            }
+            pop(); 
+        }
+    }
+    
+    return vazia(); 
+}
+
+int main() {
+    char* testes[] = {
+        "( ( ) )", 
+        "( ( )( ) )", 
+        "( ) ( )", 
+        ")(", 
+        "( ( ) (", 
+        ") ) ( ("
+    };
+
+    for (int i = 0; i < 6; i++) {
+        if (verifica_parenteses(testes[i])) {
+            printf("%s -> Correto\n", testes[i]);
+        } else {
+            printf("%s -> Incorreto\n", testes[i]);
+        }
+    }
+
+    return 0;
+}
