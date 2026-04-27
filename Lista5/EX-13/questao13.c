a8e265a7e32aa91a2e1177f27b945d20ef953f58
Augusto <augustowrcc@gmail.com>
2026-04-27 17:58:12 -0400

    Dada as operações de pilha insere I e remove R, escreva a configuração final da pilha após as seguintes operações: I(10), I(20), R, I(30), I(45), I(21), R, R

--- Lista5/EX-13/questao13.c
+++ Lista5/EX-13/questao13.c
@@ -4,5 +4,22 @@ Matrícula: 54880
 Lista: 5 
 Exercício: 13
 
+I(10) -> Pilha: [10]
+
+I(20) -> Pilha: [10, 20]
+
+R -> Pilha: [10]
+
+I(30) -> Pilha: [10, 30]
+
+I(45) -> Pilha: [10, 30, 45]
+
+I(21) -> Pilha: [10, 30, 45, 21]
+
+R -> Pilha: [10, 30, 45]
+
+R -> Pilha: [10, 30]
+
+Configuração final da pilha (do fundo para o topo): 10, 30 (onde 30 é o topo).
 */
 
