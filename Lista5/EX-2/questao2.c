25cc710211bd3ff23fc2779daec1890a41c7a72a
Augusto <augustowrcc@gmail.com>
2026-04-27 17:40:44 -0400

    Em que situações uma pilha pode ser utilizada?

--- Lista5/EX-2/questao2.c
+++ Lista5/EX-2/questao2.c
@@ -4,5 +4,19 @@ Matrícula: 54880
 Lista: 5 
 Exercício: 2
 
+
+
+
+Situações de uso de uma pilha:
+
+Call Stack: Gerenciar a execução e o retorno de funções na memória.
+
+Validação: Checar se parênteses, chaves ou tags fecham corretamente.
+
+Backtracking: "Voltar atrás" em algoritmos como resolução de labirintos.
+
+Busca (DFS): Explorar caminhos profundos em grafos ou árvores.
+
+Expressões Matemáticas: Calcular a ordem de precedência em compiladores.
 */
 
