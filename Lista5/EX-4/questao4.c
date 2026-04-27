20c62c7f61fef90bf7d5693d7cda78d411f31014
Augusto <augustowrcc@gmail.com>
2026-04-27 17:44:13 -0400

    Escreva um algoritmo, usando uma Pilha, que inverte as letras de cada palavra de um texto terminado por ponto (.) preservando a ordem das palavras. Por exemplo, dado o texto: ESTE EXERCÍCIO É MUITO FÁCIL. A saída deve ser: ETSE OICÍCREXE É OTIUM LICÁF

--- Lista5/EX-4/questao4.c
+++ Lista5/EX-4/questao4.c
@@ -6,3 +6,29 @@ Exercício: 4
 
 */
 
+#include <stdio.h>
+
+char pilha[100];
+int topo = -1;
+
+void push(char c) { pilha[++topo] = c; }
+char pop() { return pilha[topo--]; }
+int vazia() { return topo == -1; }
+
+int main() {
+    char c;
+    printf("Digite uma frase (termina com '.'): ");
+    while ((c = getchar()) != '\n' && c != EOF) {
+        if (c != ' ' && c != '.') {
+            push(c); 
+        } else {
+            while (!vazia()) {
+                putchar(pop());
+            }
+            putchar(c); 
+            
+            if (c == '.') break; 
+        }
+    }
+    return 0;
+}
