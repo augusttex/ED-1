a026a0d43c477b0dc536905a29bc6def4e762b15
Augusto <augustowrcc@gmail.com>
2026-04-27 17:55:41 -0400

    11. Escreva um programa que acumula n valores da sequencia de Fibonacci em uma pilha

--- Lista5/EX-11/questao11.c
+++ Lista5/EX-11/questao11.c
@@ -6,3 +6,42 @@ Exercício: 11
 
 */
 
+#include <stdio.h>
+
+typedef struct {
+    int itens[100];
+    int topo;
+} Pilha;
+
+void inicializar(Pilha *p) { p->topo = -1; }
+void push(Pilha *p, int v) { p->itens[++(p->topo)] = v; }
+int pop(Pilha *p) { return p->itens[(p->topo)--]; }
+int vazia(Pilha *p) { return p->topo == -1; }
+
+int main() {
+    Pilha p;
+    inicializar(&p);
+    printf("Digite o numero de termos da sequencia de Fibonacci: ");
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 0;
+    }
+    
+    if (n >= 1) push(&p, 0);
+    if (n >= 2) push(&p, 1);
+    
+    int a = 0, b = 1, proximo;
+    for (int i = 2; i < n; i++) {
+        proximo = a + b;
+        push(&p, proximo);
+        a = b;
+        b = proximo;
+    }
+    
+    while (!vazia(&p)) {
+        printf("%d ", pop(&p));
+    }
+    printf("\n");
+    
+    return 0;
+}
