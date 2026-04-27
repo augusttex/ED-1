48e8155c913b5f0b4452a8483dab48c6a7a10fc9
Augusto <augustowrcc@gmail.com>
2026-04-27 17:59:51 -0400

    A conversão de números inteiros, na base 10, para outras bases numéricas se dá através de sucessivas divisões de um dado valor n pelo valor da base na qual se queira converter. Faça um programa para obter a conversão numérica, de acordo com a opção do usuário, utilizando uma pilha: (a) Decimal para Binário. (b) Decimal para Octal. (c) Decimal para Hexadecimal

--- Lista5/EX-14/questao14.c
+++ Lista5/EX-14/questao14.c
@@ -6,3 +6,59 @@ Exercício: 14
 
 */
 
+#include <stdio.h>
+
+typedef struct {
+    int itens[100];
+    int topo;
+} Pilha;
+
+void inicializar(Pilha *p) { p->topo = -1; }
+void push(Pilha *p, int v) { p->itens[++(p->topo)] = v; }
+int pop(Pilha *p) { return p->itens[(p->topo)--]; }
+int vazia(Pilha *p) { return p->topo == -1; }
+
+void converter(int num, int base) {
+    Pilha p;
+    inicializar(&p);
+    
+    if (num == 0) {
+        printf("0\n");
+        return;
+    }
+
+    while (num > 0) {
+        push(&p, num % base);
+        num /= base;
+    }
+
+    while (!vazia(&p)) {
+        int resto = pop(&p);
+        if (resto < 10) {
+            printf("%d", resto);
+        } else {
+            printf("%c", 55 + resto);
+        }
+    }
+    printf("\n");
+}
+
+int main() {
+    int num, opcao;
+    
+    printf("Digite o numero: ");
+    if (scanf("%d", &num) != 1) return 0;
+
+    printf("1: Binario\n2: Octal\n3: Hexadecimal\nOpcao: ");
+    if (scanf("%d", &opcao) != 1) return 0;
+
+    if (opcao == 1) {
+        converter(num, 2);
+    } else if (opcao == 2) {
+        converter(num, 8);
+    } else if (opcao == 3) {
+        converter(num, 16);
+    }
+
+    return 0;
+}
