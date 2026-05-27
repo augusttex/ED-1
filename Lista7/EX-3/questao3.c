98f74cd2d780310cde9f70e4e4c21ca66bc1fe2c
Augusto <augustowrcc@gmail.com>
2026-05-27 15:14:55 -0400

    3. Escreva uma função que conta o número de folhas de uma árvore binária.

---
+++ Lista7/EX-3/questao3.c
@@ -0,0 +1,132 @@
+/*Nome: Augusto Castilho
+Matrícula: 54880
+Lista: 7
+Exercício: 3
+
+*/ 
+
+
+#include <stdio.h>
+#include<stdlib.h>
+
+struct NO{
+    int info;
+    struct NO *esq;
+    struct NO *dir;
+};
+
+typedef struct NO* ArvBin;
+
+ArvBin* cria_ArvBin(){
+    ArvBin* raiz = (ArvBin*)malloc(sizeof(ArvBin));
+    if(raiz != NULL)
+        *raiz = NULL;
+    return raiz;
+}
+
+
+void preOrdem_ArvBin(ArvBin *raiz){
+    if(raiz == NULL)
+        return;
+    if(*raiz != NULL){
+        printf("%d\n", (*raiz)->info);
+        preOrdem_ArvBin(&((*raiz)->esq));
+        preOrdem_ArvBin(&((*raiz)->dir));
+    }
+}
+
+int insere_ArvBin(ArvBin* raiz, int valor){
+    if(raiz == NULL)
+        return 0;
+    struct NO* novo;
+    novo = (struct NO*)malloc(sizeof(struct NO));
+    if(novo == NULL)
+        return 0;
+    novo->info = valor;
+    novo->dir = NULL;
+    novo->esq = NULL;
+    if(*raiz == NULL)
+        *raiz = novo;
+    else{
+        struct NO* atual = *raiz;
+        struct NO* ant = NULL;
+        while(atual != NULL){
+            ant = atual;
+            if(valor == atual->info){
+                free(novo);
+                return 0; // elemento ja existe
+            }
+            if(valor > atual->info)
+                atual = atual->dir;
+            else
+                atual = atual->esq;
+        }
+        if(valor > ant->info)
+            ant->dir = novo;
+        else
+            ant->esq = novo;
+    }
+    return 1;
+}
+
+
+int contarNos_ArvBin(ArvBin *raiz){
+
+    struct NO* pilha[100];
+    int topo = 0;
+    int nos = 0;
+    
+    if(raiz == NULL)
+    return 0;
+
+    pilha[topo] = *raiz;
+    topo++;
+    
+    while(topo > 0)
+    {
+        struct NO* atual = pilha[--topo];
+        nos++;
+        if(atual->dir != NULL)
+            pilha[topo++] = atual->dir;
+        if(atual->esq != NULL)
+            pilha[topo++] = atual->esq;
+    }
+
+
+    return nos;
+}
+
+int totalNO_ArvBin(ArvBin *raiz){
+    if(raiz == NULL || *raiz == NULL)
+        return 0;
+    int alt_esq = totalNO_ArvBin(&((*raiz)->esq));
+    int alt_dir = totalNO_ArvBin(&((*raiz)->dir));
+    return (alt_esq + alt_dir + 1); 
+}
+
+int folhaNO_ArvBin(ArvBin *raiz){
+    if(raiz == NULL || *raiz == NULL)
+        return 0;
+    if((*raiz)->esq == NULL && (*raiz)->dir == NULL)
+     return 1;
+     else{
+    int alt_esq = folhaNO_ArvBin(&((*raiz)->esq));
+    int alt_dir = folhaNO_ArvBin(&((*raiz)->dir));
+    return alt_dir + alt_esq;
+     } 
+}
+
+int main()
+{
+    ArvBin *raiz = cria_ArvBin();
+
+    insere_ArvBin(raiz, 50);
+    insere_ArvBin(raiz, 30);
+    insere_ArvBin(raiz, 70);
+    insere_ArvBin(raiz, 20);
+    insere_ArvBin(raiz, 40);
+
+    printf("O numero de folha nessa arvore e: %d ", folhaNO_ArvBin(raiz));
+    return 0;
+    
+}
