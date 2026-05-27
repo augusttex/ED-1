66fddf17add37343b17e5421d797ecf1b69a7b90
Augusto <augustowrcc@gmail.com>
2026-05-27 16:59:07 -0400

    7. Escreva uma função que encontra o valor mínimo em uma árvore de busca binária.

---
+++ Lista7/EX-7/questao7.c
@@ -0,0 +1,82 @@
+/*Nome: Augusto Castilho
+Matrícula: 54880
+Lista: 7
+Exercício: 7
+
+*/ 
+#include <stdio.h>
+#include<stdlib.h>
+
+struct NO{
+    int info;
+    struct NO *esq;
+    struct NO *dir;
+};
+
+typedef struct NO* ArvBin;
+
+ArvBin* cria_ArvBin(){
+    ArvBin* raiz = (ArvBin*)malloc(sizeof(ArvBin));
+    if(raiz != NULL)
+        *raiz = NULL;
+    return raiz;
+}
+
+int insere_ArvBin(ArvBin* raiz, int valor){
+    if(raiz == NULL)
+        return 0;
+    struct NO* novo;
+    novo = (struct NO*)malloc(sizeof(struct NO));
+    if(novo == NULL)
+        return 0;
+    novo->info = valor;
+    novo->dir = NULL;
+    novo->esq = NULL;
+    if(*raiz == NULL)
+        *raiz = novo;
+    else{
+        struct NO* atual = *raiz;
+        struct NO* ant = NULL;
+        while(atual != NULL){
+            ant = atual;
+            if(valor == atual->info){
+                free(novo);
+                return 0; // elemento ja existe
+            }
+            if(valor > atual->info)
+                atual = atual->dir;
+            else
+                atual = atual->esq;
+        }
+        if(valor > ant->info)
+            ant->dir = novo;
+        else
+            ant->esq = novo;
+    }
+    return 1;
+}
+
+int menorABB_ArvBin(ArvBin* raiz){
+     if(raiz == NULL || *raiz == NULL)
+        return 0;
+    
+    struct NO* atual = *raiz;
+
+    while(atual->esq != NULL)
+        atual = atual->esq;
+
+    return atual->info;
+}
+
+int main()
+{
+    ArvBin *raiz = cria_ArvBin();
+
+    insere_ArvBin(raiz, 50);
+    insere_ArvBin(raiz, 30);
+    insere_ArvBin(raiz, 70);
+    insere_ArvBin(raiz, 20);
+    insere_ArvBin(raiz, 40);
+
+    printf("\nO valor maximo e %d", menorABB_ArvBin(raiz));
+}
